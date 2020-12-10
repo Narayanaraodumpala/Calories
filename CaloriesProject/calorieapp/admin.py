25051551737202b322659cfc4e31627c9fee7ef5
Narayanaraodumpala <63154449+Narayanaraodumpala@users.noreply.github.com>
2020-12-10 12:17:43 +0530

    updated
    updated

--- CaloriesProject/calorieapp/admin.py
+++ CaloriesProject/calorieapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from  .models import *
 # Register your models here.
+
+
+
+admin.site.register(Userdetails)
+admin.site.register(FoodModel)
+admin.site.register(BMIModel)
--- CaloriesProject/calorieapp/models.py
+++ CaloriesProject/calorieapp/models.py
@@ -14,3 +14,26 @@ class BMIModel(models.Model):
     usr=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     bmi=models.CharField(max_length=20,null=True)
     date=models.DateTimeField(auto_now_add=True)
+
+
+class FoodModel(models.Model):
+      food=models.CharField(max_length=50,null=True)
+      food_type=models.CharField(max_length=30,null=True)
+      food_image=models.ImageField(upload_to='food_images/',null=True)
+
+      calories = models.FloatField(null=True)
+      water = models.FloatField(null=True)
+      protein = models.FloatField(null=True)
+      carbohydrates=models.FloatField(null=True)
+      sugar = models.FloatField(null=True)
+      fiber = models.FloatField(null=True)
+      fats=models.FloatField(null=True)
+      vitamin=models.FloatField(null=True)
+
+
+
+
+      def __str__(self):
+          return self.food
+
+
